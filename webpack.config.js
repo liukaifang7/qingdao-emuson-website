f13be41c158a2206e0226fed86e48318df9834e3
liukaifang7 <kaifang428@126.com>
2025-08-09 16:47:02 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     rules: [
       { test: /\.(js|jsx|mjs)$/, exclude: /node_modules/, use: ['babel-loader'] },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|jpe?g|gif|svg)$/i, type: 'asset/resource' },
+      { test: /\.(png|jpe?g|gif|svg|ico)$/i, type: 'asset/resource' },
     ],
   },
   resolve: {
@@ -26,33 +26,11 @@ module.exports = {
       minify: false
     }),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-      publicPath: '/',
-      serveIndex: false
-    },
-    port: process.env.PORT || 3006,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-    compress: true,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: false,
-      },
-    },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    setupMiddlewares: (middlewares, devServer) => {
-      // 禁用serve-index中间件
-      return middlewares.filter(middleware => 
-        !middleware.name || !middleware.name.includes('serve-index')
-      );
-    },
+  stats: { 
+    errorDetails: true, 
+    colors: true, 
+    chunks: false,
+    children: false
   },
-  stats: { errorDetails: true, colors: true, chunks: false },
-  mode: 'development',
+  mode: 'production',
 };
