474a97e9e5d6d3bc4eac66c4a5c7471b3416ff53
liukaifang7 <kaifang428@126.com>
2025-08-09 17:09:41 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'production',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -27,5 +28,18 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html'
     })
-  ]
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+    compress: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
