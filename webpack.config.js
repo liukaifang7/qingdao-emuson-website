2608a61f99e337ce2d96b76f4d9b1acab7101e00
liukaifang7 <kaifang428@126.com>
2025-08-09 16:42:46 +0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ 
-      template: './public/index.html', 
-      favicon: './public/favicon.ico',
+      template: './public/index.html',
       inject: true,
       minify: false
     }),
